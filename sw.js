b8b7b0c816d6cb0264f80d606d1c16e06807d237
Christopher Van <cvan@mozilla.com>
2016-10-31 14:24:32 -0700

    remove unused code in SW

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@ var debug = false;
 var log = debug ? console.log.bind(console) : function () {};
 
 var CURRENT_CACHES_HASHES = {
-  static: '-v-0a5997de1ad80b308442bf032266444b1fb50b6dbfc792b8dbcd0bc8ec40f712'  // {STATIC_HASH}
+  static: '-v-8b0c157de4bb96c9b0d0e9fcd1ba9601bfe0f8c07f14762d0ab7735cd6ca0106'  // {STATIC_HASH}
 };
 
 var CURRENT_CACHES = {
@@ -11,7 +11,7 @@ var CURRENT_CACHES = {
 };
 
 self.addEventListener('activate', function (event) {
-  console.log('WORKER: activate event in progress');
+  log('WORKER: activate event in progress');
 
   // Delete all caches that aren't named in CURRENT_CACHES.
   // While there is only one cache in this example, the same logic will handle the case where
@@ -147,58 +147,3 @@ self.addEventListener('fetch', function (event) {
       })
   );
 });
-
-// self.addEventListener('fetch', function (event) {
-//   log('Handling fetch event for URL:', event.request.url);
-
-//   event.respondWith(
-//     caches.open(CURRENT_CACHES.static).then(function (cache) {
-//       return cache.match(event.request).then(function (response) {
-//         if (response) {
-//           // If there is an entry in the cache for `event.request`, then `response` will be defined
-//           // and we can just return it. Notice that only `static` resources are cached.
-//           log(' Found response in cache:', response);
-
-//           return response;
-//         }
-
-//         // Otherwise, if there is no entry in the cache for `event.request`, `response` will be
-//         // `undefined`, and we need to `fetch()` the resource.
-//         log(' No response for %s found in cache. About to fetch ' +
-//           'from network:', event.request.url);
-
-//         // We call `.clone()` on the request since we might use it in a call to `cache.put()` later on.
-//         // Both `fetch()` and `cache.put()` "consume" the request, so we need to make a copy
-//         // (https://fetch.spec.whatwg.org/#dom-request-clone).
-//         return fetch(event.request.clone()).then(function (response) {
-//           log('  Response for %s from network: %O',
-//             event.request.url, event.request.url);
-//           if (response.status < 400 &&
-//               response.type === 'basic') {
-//             // This avoids caching responses that we know are errors (i.e., 4xx/5xx HTTP status codes).
-//             // Note that for opaque filtered responses (https://fetch.spec.whatwg.org/#concept-filtered-response-opaque),
-//             // we can't access to the response headers, so this check will always fail and the resource won't be cached.
-//             // (Reminder: cross-origin requests must serve CORS headers.)
-//             // We call `.clone()` on the response to save a copy of it to the cache. By doing so, we get to keep
-//             // the original response object which we will return back to the controlled page.
-//             // (https://fetch.spec.whatwg.org/#dom-response-clone).
-//             log('  Caching the response for URL:', event.request.url);
-//             cache.put(event.request, response.clone());
-//           } else {
-//             console.log('  Not caching the response URL:', event.request.url);
-//           }
-
-//           // Return the original response object, which will be used to fulfill the resource request.
-//           return response;
-//         });
-//       }).catch(function (err) {
-//         // This `catch()` will handle exceptions that arise from the `match()` or `fetch()` operations.
-//         // Note that a HTTP error response (e.g., 404) will *not* trigger an exception.
-//         // It will return a normal response object that has the appropriate error code set.
-//         console.error('  Error in fetch handler:', err);
-
-//         throw err;
-//       });
-//     })
-//   );
-// });
